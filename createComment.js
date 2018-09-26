6efb3a83f7ac2bfa69e0aa20dae5249aeab91d39
Egor <zangedzu333gig@gmail.com>
2018-09-26 18:25:23 +0300

    Task 00

---
+++ createComment.js
@@ -0,0 +1,17 @@
+const log = require('./log').log;
+let articles = require('./articles.json');
+
+module.exports.createComment = function createComment(req, res, payload, cb) {
+	if (payload.articleId == undefined) cb({ code: 400, message: 'Invalid request'});
+	else {
+	    let ind = articles.findIndex(i => i.id == payload.articleId);
+	    if (ind != -1) {
+	    	log('/api/comments/create', payload);
+	        payload.id = Date.now();
+	        if (articles[ind].comments == undefined) articles[ind].comments = [];
+	        articles[ind].comments.push(payload);
+	        cb(null, articles);
+	    }
+	    else cb({ code: 400, message: 'Invalid request' });
+	}
+}
